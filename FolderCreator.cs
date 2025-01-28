649197a84ccf6624e544cde98efa675ce21bd9f0
efekantgn <56132486+efekantgn@users.noreply.github.com>
2025-01-28 09:26:11 +0300

    Init

---
+++ FolderCreator.cs
@@ -0,0 +1,56 @@
+using UnityEditor;
+using UnityEngine;
+
+public class FolderCreator : EditorWindow
+{
+    [MenuItem("Tools/Create Project Folders")]
+    public static void ShowWindow()
+    {
+        GetWindow<FolderCreator>("Folder Creator");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Create Default Folder Structure", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Create Folders"))
+        {
+            CreateFolders();
+        }
+    }
+
+    private void CreateFolders()
+    {
+        string[] folders =
+        {
+            "Animations",
+            "Fonts",
+            "Materials",
+            "Models",
+            "Prefabs",
+            "Scenes",
+            "Scripts",
+            "Shaders",
+            "Sprites",
+            "StorePackages",
+            "TextMeshPro",
+            "Textures"
+        };
+
+        foreach (string folder in folders)
+        {
+            string folderPath = $"Assets/{folder}";
+            if (!AssetDatabase.IsValidFolder(folderPath))
+            {
+                AssetDatabase.CreateFolder("Assets", folder);
+                Debug.Log($"Created folder: {folderPath}");
+            }
+            else
+            {
+                Debug.LogWarning($"Folder already exists: {folderPath}");
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
